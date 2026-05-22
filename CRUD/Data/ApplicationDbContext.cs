6d5198e38a1946e652b08ad0a86f4f5fdbaa977d
Priyank Maisuriya <priyankmaisuriya7@gmail.com>
2026-05-22 12:28:33 +0530

    CRUD
    ADD CRUD opration

---
+++ CRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CRUD/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display(Name = "Student ID")]
+        public int StudentId { get; set; }
+
+        [Display(Name = "Student Name")]
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Course { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
